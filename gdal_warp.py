f8a1df4badaa5344324c846891c1e133d54d207e
Juanjolizard <41548034+Juanjolizard@users.noreply.github.com>
2021-09-29 22:27:50 +0100

    GDAl Warp Reproject
    Reproject raster data in .asc format to EPSG:32628.

---
+++ gdal_warp.py
@@ -0,0 +1,29 @@
+import processing
+import os
+import glob
+
+src_folder = 'C:/capas_nocrs'
+dst_folder = 'C:/prueba_python'
+
+rasters = glob.glob(src_folder + '/*.asc')
+#print (rasters)
+
+for raster in rasters:
+    print(raster)
+    dst_name = os.path.splitext(os.path.basename(raster))[0]
+    parameters = {'DATA_TYPE' : 0,
+    'EXTRA' : '', 
+    'INPUT' : raster, 
+    'MULTITHREADING' : False,
+    'NODATA' : -9999, 
+    'OPTIONS' : '', 
+    'OUTPUT' : os.path.join(dst_folder,f'{dst_name}.asc'),
+    'RESAMPLING' : 0, 
+    'SOURCE_CRS' : None, 
+    'TARGET_CRS' : 'EPSG:32628',
+    'TARGET_EXTENT' : None, 
+    'TARGET_EXTENT_CRS' : None, 
+    'TARGET_RESOLUTION' : None}
+    feedback = QgsProcessingFeedback()
+    processing.runAndLoadResults('gdal:warpreproject',parameters,feedback=feedback)
+    print("Se ha reproyectado la capa" + ' ' + f'{dst_name}.asc')##
